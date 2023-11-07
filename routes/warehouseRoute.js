14aaea6ae97f70326ad6a059f7b1627b346bf2ef
Narmadaw <narmadaw9@gmail.com>
2023-11-07 01:40:38 -0800

    get warehouse list, by id and create warehouse

---
+++ routes/warehouseRoute.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const warehouseController = require("./../controllers/warehouseController");
+
+router.route('/').get(warehouseController.getWarehouseList);
+router.route("/:id").get(warehouseController.getWarehouseById);
+router.route('/').post(warehouseController.createWarehouse);
+
+  
+module.exports = router;
