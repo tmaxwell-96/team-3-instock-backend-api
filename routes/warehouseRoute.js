efd484b423894f9fe10dad6b913294c65a91c60f
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-07 19:32:49 -0800

    Add SVT-18/put warehouse endpoint

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const warehouseController = require("./../controllers/warehouseController");
 
-router.route('/').get(warehouseController.getWarehouseList);
-router.route("/:id").get(warehouseController.getWarehouseById);
-router.route('/').post(warehouseController.createWarehouse);
+router.route("/").get(warehouseController.getWarehouseList);
+router
+  .route("/:id")
+  .get(warehouseController.getWarehouseById)
+  .put(warehouseController.changeWarehouse);
+router.route("/").post(warehouseController.createWarehouse);
 
-  
 module.exports = router;
