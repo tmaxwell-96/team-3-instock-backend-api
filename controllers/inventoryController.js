d869667a2648f254a0785f9ae074f141a68e6025
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-08 09:45:54 -0800

    Complete POST route for inventory

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -5,6 +5,11 @@ const createInventory = async (req, res) => {
   try {
     console.log(req.body);
     const newInventoryData = await knex("inventories").insert(req.body);
+    const newInventoryId = newInventoryData[0];
+    const createdInventory = await knex("inventories").where({
+      id: newInventoryId,
+    });
+    res.status(201).json(createdInventory);
   } catch (error) {
     res.status(500).json({
       message: `Unable to create new inventory item:`,
