148e83efac0b4dba47e3bf7efa3964e16540707c
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-08 16:06:53 -0800

    Reconfigure inventoryrouter for new routes

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,33 +1,37 @@
 const knex = require("knex")(require("../knexfile"));
-const getInventoryById = async (req, res) => {
+
+//GET a list of all inventory items
+const getAllInventory = async (_req, res) => {
   try {
-    const data = await knex("inventories").where({
-      id: req.params.id,
-    });
+    const data = await knex("inventories")
+      .join("warehouses", "warehouses.id", "inventories.warehouse_id")
+      .select("inventories.*", "warehouse_name as warehouse_name"); // Correct the alias here
+
     res.status(200).json(data);
   } catch (error) {
-    res.status(400).send("Error retrieving Inventories: error");
+    console.error(error); // Log the error for debugging
+    res.status(400).send("Error getting inventory list");
   }
 };
 
-const getInventoryListByWarehouseById = async (req, res) => {
+//POST Create new inventory item
+const createInventory = async (req, res) => {
   try {
-    const InventoryFound = await knex("inventories").where({
-      warehouse_id: req.params.id,
+    console.log(req.body);
+    const newInventoryData = await knex("inventories").insert(req.body);
+    const newInventoryId = newInventoryData[0];
+    const createdInventory = await knex("inventories").where({
+      id: newInventoryId,
     });
-
-    if (InventoryFound.length === 0) {
-      return res.status(404).json({
-        message: `Item with ID ${req.params.id} not found`,
-      });
-    }
-    const InventoryData = InventoryFound;
-    res.json(InventoryData);
+    res.status(201).json(createdInventory);
   } catch (error) {
     res.status(500).json({
-      message: `Unable to retrieve warehouse data for warehouse with ID ${req.params.id}`,
+      message: `Unable to create new inventory item:`,
     });
   }
 };
 
-module.exports = { getInventoryListByWarehouseById };
+module.exports = {
+  createInventory,
+  getAllInventory,
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,13 +1,6 @@
 const router = require("express").Router();
 const inventoryController = require("./../controllers/inventoryController");
 
-router.route("/").get(inventoryController.getInventoryById);
-router.route("/:id").get(inventoryController.getInventoryById);
-router
-  .route("/warehouses/:id/inventories")
-  .get(inventoryController.getInventoryListByWarehouseById);
-router.route("/").post(inventoryController.createInventory);
-
 router
   .route("/")
   .get(inventoryController.getAllInventory)
