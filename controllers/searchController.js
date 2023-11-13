e5ae992bd7140284ba734987998ea4ea4037ea73
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-12 19:47:58 -0800

    Correct search function selector

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,27 +1,36 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
-const searchWarehouseOrInventory = (endpoint, searchKeyword) =>{
-    const tableName = endpoint === '/warehouses' ? 'warehouses' : 'inventories';
-    const searchFields = endpoint === '/warehouses'
-      ? ['warehouse_name', 'address', 'city', 'country', 'contact_name', 'contact_phone','contact_email' ]
-      : ['item_name', 'warehouse_name', 'category', 'description'];
+const searchWarehouseOrInventory = (endpoint, searchKeyword) => {
+  const tableName = endpoint === "/warehouses" ? "warehouses" : "inventories";
+  const searchFields =
+    endpoint === "/warehouses"
+      ? [
+          "warehouse_name",
+          "address",
+          "city",
+          "country",
+          "contact_name",
+          "contact_phone",
+          "contact_email",
+        ]
+      : ["item_name", "warehouse_name", "category", "description"];
 
-      let query = knex(tableName).select('*');
+  let query = knex(tableName).select("*");
 
-      if (tableName === 'inventories') {
-        query = query
-          .innerJoin('warehouses', 'inventories.warehouse_id', 'warehouses.id');
+  if (tableName === "inventories") {
+    query = query
+      .innerJoin("warehouses", "warehouses.id", "inventories.warehouse_id")
+      .select("inventories.*", "warehouse_name as warehouse_name");
+  }
+
+  return query.where((builder) => {
+    builder.orWhere(function () {
+      for (const field of searchFields) {
+        this.orWhere(field, "like", `%${searchKeyword}%`);
       }
-  
-    return query
-      .where((builder) => {
-        builder.orWhere(function () {
-          for (const field of searchFields) {
-            this.orWhere(field, 'like', `%${searchKeyword}%`);
-          }
-        });
-      });
-}
+    });
+  });
+};
 module.exports = {
-    searchWarehouseOrInventory,
-  };
+  searchWarehouseOrInventory,
+};
